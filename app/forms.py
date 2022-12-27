d1820508af02d14f162f4930fb126592c1c80e5b
gonzalo-cordova-pou <gcp@vincesconsulting.com>
2022-12-28 00:53:47 +0100

    feat: adding reset button to form

--- app/forms.py
+++ app/forms.py
@@ -5,4 +5,5 @@ from wtforms.validators import DataRequired, Length
 class InputForm(FlaskForm):
     """Form to submit input to the interpreter."""
     input = StringField('Input', validators=[DataRequired(), Length(min=1, max=20000)])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Run')
+    reset = SubmitField('Reset')
